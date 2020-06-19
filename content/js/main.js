194ad95e772437abc856ac098b3cb0c661342d90
¨pdellasera¨ <pdellasera@gmail.com>
2020-06-19 00:12:20 -0500

    change

--- content/js/main.js
+++ content/js/main.js
@@ -1,6 +1,6 @@
 /* Only register a service worker if it's supported */
 if ('serviceWorker' in navigator) {
-         navigator.serviceWorker.register('./sw.js');
+         navigator.serviceWorker.register('./sw/sw.js');
 }
 // if (window.location.protocol === 'http:') {
 //     const requireHTTPS = document.getElementById('requireHTTPS');
@@ -8,38 +8,6 @@ if ('serviceWorker' in navigator) {
 //     link.href = window.location.href.replace('http://', 'https://');
 //     requireHTTPS.classList.remove('hidden');
 //   }
-const divInstall = document.getElementById('installContainer');
-const butInstall = document.getElementById('butInstall');
-window.addEventListener('beforeinstallprompt', (event) => {
-    console.log('👍', 'beforeinstallprompt', event);
-    // Stash the event so it can be triggered later.
-    window.deferredPrompt = event;
-    // Remove the 'hidden' class from the install button container
-    divInstall.classList.toggle('hidden', false);
-  });
-
-  butInstall.addEventListener('click', () => {
-    console.log('👍', 'butInstall-clicked');
-    const promptEvent = window.deferredPrompt;
-    if (!promptEvent) {
-      // The deferred prompt isn't available.
-      return;
-    }
-    // Show the install prompt.
-    promptEvent.prompt();
-    // Log the result
-    promptEvent.userChoice.then((result) => {
-      console.log('👍', 'userChoice', result);
-      // Reset the deferred prompt variable, since
-      // prompt() can only be called once.
-      window.deferredPrompt = null;
-      // Hide the install button.
-      divInstall.classList.toggle('hidden', true);
-    });
-  });
-  window.addEventListener('appinstalled', (event) => {
-    console.log('👍', 'appinstalled', event);
-  });
 document.onreadystatechange = function () {
     var state = document.readyState;
     if (state == 'complete') {
